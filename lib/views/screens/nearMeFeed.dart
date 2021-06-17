39b5fd4505ec83758d3ac73a9791c5b0ce5a3f04
shehan82 <shehansandeepa82@gmail.com>
2021-06-18 00:43:30 +0530

    sigitiya feed UI almost done

--- lib/views/screens/nearMeFeed.dart
+++ lib/views/screens/nearMeFeed.dart
@@ -10,6 +10,7 @@ class _NearMeFeedScreenState extends State<NearMeFeedScreen> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      // crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
           child: Stack(
@@ -143,6 +144,211 @@ class _NearMeFeedScreenState extends State<NearMeFeedScreen> {
                     ),
                   ))
             ],
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+            child: Text(
+              "Nearest Destinations",
+              style: GoogleFonts.montserrat(fontSize: 15, color: Colors.white),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.green[800],
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    bottomRight: Radius.circular(20))),
+          ),
+        ),
+
+        // Image(image: AssetImage('assets/images/polo.jpg'))
+        Container(
+          margin: EdgeInsets.only(top: 20),
+          child: Container(
+            child: Stack(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  height: 180,
+                  // width: (MediaQuery.of(context).size.width / 100) * 92,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/polo.jpg'),
+                          fit: BoxFit.fitWidth)),
+                ),
+                Positioned(
+                    height: 40,
+                    bottom: 10,
+                    left: 20,
+                    right: 20,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.7),
+                          borderRadius: BorderRadius.circular(10)),
+                      width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      height: 20,
+                      child: Container(
+                          child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                              child: Text(
+                            "Polonnaruwa",
+                            style: GoogleFonts.montserrat(
+                                fontSize: 15,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500),
+                          )),
+                          Container(
+                            child: Row(
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.only(right: 5),
+                                  child: Icon(
+                                    Icons.star,
+                                    color: Colors.amber[300],
+                                    size: 16,
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(right: 5),
+                                  child: Icon(
+                                    Icons.star,
+                                    color: Colors.amber[300],
+                                    size: 16,
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(right: 5),
+                                  child: Icon(
+                                    Icons.star,
+                                    color: Colors.amber[300],
+                                    size: 16,
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(right: 5),
+                                  child: Icon(
+                                    Icons.star,
+                                    color: Colors.amber[300],
+                                    size: 16,
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(right: 5),
+                                  child: Icon(
+                                    Icons.star,
+                                    color: Colors.amber[300],
+                                    size: 16,
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      )),
+                    ))
+              ],
+            ),
+          ),
+        ),
+        // ksdhfkj
+        Container(
+          margin: EdgeInsets.only(top: 20),
+          child: Container(
+            child: Stack(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  height: 180,
+                  // width: (MediaQuery.of(context).size.width / 100) * 92,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/dambulla.jpg'),
+                          fit: BoxFit.fitWidth)),
+                ),
+                Positioned(
+                    height: 40,
+                    bottom: 10,
+                    left: 20,
+                    right: 20,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.black.withOpacity(0.7),
+                          borderRadius: BorderRadius.circular(10)),
+                      width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      height: 20,
+                      child: Container(
+                          child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                              child: Text(
+                            "Dambulla cave Temple",
+                            style: GoogleFonts.montserrat(
+                                fontSize: 15,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500),
+                          )),
+                          Container(
+                            child: Row(
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.only(right: 5),
+                                  child: Icon(
+                                    Icons.star,
+                                    color: Colors.amber[300],
+                                    size: 16,
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(right: 5),
+                                  child: Icon(
+                                    Icons.star,
+                                    color: Colors.amber[300],
+                                    size: 16,
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(right: 5),
+                                  child: Icon(
+                                    Icons.star,
+                                    color: Colors.amber[300],
+                                    size: 16,
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(right: 5),
+                                  child: Icon(
+                                    Icons.star,
+                                    color: Colors.amber[300],
+                                    size: 16,
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(right: 5),
+                                  child: Icon(
+                                    Icons.star,
+                                    color: Colors.amber[300],
+                                    size: 16,
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      )),
+                    ))
+              ],
+            ),
           ),
         ),
         SizedBox(
