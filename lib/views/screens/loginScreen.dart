39b030051411dbec3ae1bb7d1dfbb42e2d508a93
deeghayu <deefernando6@gmail.com>
2021-07-06 22:17:21 +0530

    Update loginScreen.dart

--- lib/views/screens/loginScreen.dart
+++ lib/views/screens/loginScreen.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+class LoginScreen extends StatefulWidget {
+  // const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
