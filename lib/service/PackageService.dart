16d5aab4ad6014fd265ea89dc75c345988b6b07d
shashiradw <shashiradw7@gmail.com>
2021-09-11 17:10:07 +0530

    Hotel packages load function

--- lib/service/PackageService.dart
+++ lib/service/PackageService.dart
@@ -1,7 +1,6 @@
-
 import 'dart:convert';
-
-import 'package:ceyntra_mobile/models/PackageModel.dart';
+import 'package:ceyntra_mobile/models/HotelPackageModel.dart';
+import 'package:ceyntra_mobile/models/TaxiPackageModel.dart';
 import 'package:ceyntra_mobile/service/UserService.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,7 +8,7 @@ import 'package:http/http.dart' as http;
 class PackageService{
 
 //  Taxis
-  Future<List<PackageModel>> loadTaxiPackages(int userID) async {
+  Future<List<TaxiPackageModel>> loadTaxiPackages(int userID) async {
 
 
       http.Response response = await http.get(
@@ -20,14 +19,41 @@ class PackageService{
 
       var listJson=packageJson as List;
 
-      List<PackageModel> packages= [];
+      List<TaxiPackageModel> packages= [];
 
       packages = listJson
-          .map((package) => PackageModel.fromJson(package))
+          .map((package) => TaxiPackageModel.fromJson(package))
           .toList();
 
       return packages;
   }
+
+  //Hotel
+
+  Future<List<HotelPackageModel>> loadHotelPackages(int userID) async {
+
+
+    http.Response response = await http.get(
+        Uri.parse('http://10.0.2.2:9092/hotelPackages/$userID')  //$userID
+    );
+
+    final packageJson = json.decode(response.body);
+
+    var listJson=packageJson as List;
+
+    List<HotelPackageModel> packages= [];
+
+    packages = listJson
+        .map((package) => HotelPackageModel.fromJson(package))
+        .toList();
+
+    print("Hotels");
+    print(packages);
+
+    return packages;
+  }
+
+
 
 
 
