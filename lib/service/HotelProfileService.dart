bcf18cf13d5a611a016fc272975abe0a7b10c6f9
HiruniHasara <63002515+HiruniHasara@users.noreply.github.com>
2021-09-13 17:47:47 +0530

    added needed dio requests

---
+++ lib/service/HotelProfileService.dart
@@ -0,0 +1,26 @@
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HotelProfileService{
+  var dio=Dio();
+
+  Future<int> getHotelUsertId() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int id = prefs.getInt("userID");
+    return id;
+  }
+
+  void getHotelProfileDetails(Function getHotelProfileData, uID) async{
+    var response= await dio.get('http://10.0.2.2:9092/hotelUserProfile/$uID');
+    getHotelProfileData(response);
+  }
+
+  Future<int> updateHotelProfilePhoto(photo, uID) async{
+    Map<String, dynamic> photoData = {
+      "photo": photo,
+      "userID": uID,
+    };
+    var response = await dio.put('http://10.0.2.2:9092/updateHotelProfilePhoto', data: photoData);
+    return response.data;
+  }
+}
