c2122c7bbc9304ff98cebb72d5d111a3c6bee42e
shashiradw <shashiradw7@gmail.com>
2021-09-13 23:13:31 +0530

    ForgetPassword page renamed

--- lib/views/screens/forgetPassword/newPassword.dart
+++ lib/views/screens/forgetPassword/newPassword.dart
@@ -1,172 +1,224 @@
+import 'package:ceyntra_mobile/service/ForgetPasswordService.dart';
+import 'package:ceyntra_mobile/views/screens/loginScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class ForgetPassword extends StatefulWidget {
-  //ForgetPassword({Key? key}) : super(key: key);
+class NewPassword extends StatefulWidget {
+
+  NewPassword({Key key,@required this.pin,@required this.email}) : super(key: key);
+  final String pin;
+  final String email;
 
   @override
-  _ForgetPasswordState createState() => _ForgetPasswordState();
+  _NewPasswordState createState() => _NewPasswordState();
 }
 
-class _ForgetPasswordState extends State<ForgetPassword> {
+class _NewPasswordState extends State<NewPassword> {
+
+  bool _isObscure1=true;
+  bool _isObscure2=true;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String password;
+  String confPassword;
+  ForgetPasswordService forgetPasswordService=new ForgetPasswordService();
+  bool notMatched=false;
+  bool updateFailed=false;
+
+  updatePassword(BuildContext context) async {
+
+    bool updated= await forgetPasswordService.updatePassword(password, widget.email, widget.pin);
+
+    print("Updated : "+ updated.toString());
+
+    if(! updated){
+      setState(() {
+        updateFailed=true;
+      });
+    }else{
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => LoginScreen(passwordReset: "Success",)));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xff2F3546),
-        body: SafeArea(
-          child: ListView(
-            physics: ClampingScrollPhysics(),
-            children: [
-              //1st Login
-              Container(
-                alignment: Alignment.center,
-                padding: EdgeInsets.only(bottom: 25.0, top: 40.0),
-                child: Text(
-                  "Change Password",
-                  style: GoogleFonts.montserrat(
-                    color: Colors.white,
-                    fontSize: 26.0,
-                    fontWeight: FontWeight.w700,
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0.0,
+        leading: InkWell(
+          onTap: (){
+            Navigator.pop(context);
+          },
+          child: Icon(Icons.arrow_back),
+        ),
+        backgroundColor: Color(0xff031925),
+      ),
+
+        backgroundColor: Color(0xff031925),
+        body: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20.0,
+            vertical: 20.0,
+          ),
+          child: SafeArea(
+
+            child: ListView(
+              physics: ClampingScrollPhysics(),
+              children: [
+
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "New Password",
+                    style: GoogleFonts.montserrat(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 ),
-              ),
 
-              //btn-bar
+                SizedBox(height: 40,),
 
-              //login with email text
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      //New Password
+                      TextFormField(
+                        validator: (val) {
+                          return val.length >6 ? null : "Password must be > 6 digits";
+                        },
 
-              Container(
-                padding: EdgeInsets.only(
-                    left: 15.0, right: 15.0, top: 40.0, bottom: 50.0),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        hintText: "New Password",
-                        filled: true,
-                        contentPadding: EdgeInsets.only(
-                            left: 15.0, right: 15.0, top: 5.0, bottom: 10.0),
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                            borderRadius: const BorderRadius.all(
-                                const Radius.circular(10.0))),
+                        onSaved: (val){
+                          password=val;
+                        },
+
+                        decoration: InputDecoration(
+                          hintText: "New Password",
+                          filled: true,
+                          contentPadding: EdgeInsets.only(
+                              left: 15.0, right: 15.0, top: 5.0, bottom: 10.0),
+                          fillColor: Colors.white,
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _isObscure1 ? Icons.visibility : Icons.visibility_off,
+                            ),
+                            onPressed: (){
+                              setState(() {
+                                _isObscure1=!_isObscure1;
+                              });
+                            },
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: const BorderRadius.all(
+                                  const Radius.circular(10.0))),
+                        ),
+                        obscureText: _isObscure1,
                       ),
-                    ),
-                    Padding(padding: EdgeInsets.only(bottom: 25.0)),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        filled: true,
-                        hintText: "Cnnfirm Password",
-                        contentPadding: EdgeInsets.all(15.0),
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                            borderRadius: const BorderRadius.all(
-                                const Radius.circular(10.0))),
+
+                      SizedBox(height: 25,),
+
+                      //Confirm Password
+                      TextFormField(
+
+                        onSaved: (val){
+                          confPassword=val;
+                        },
+
+                        decoration: InputDecoration(
+                          filled: true,
+                          hintText: "Confirm Password",
+                          contentPadding: EdgeInsets.all(15.0),
+                          fillColor: Colors.white,
+
+                          suffixIcon: IconButton(
+                              icon: Icon(
+                                _isObscure2 ? Icons.visibility : Icons.visibility_off,
+                              ),
+                            onPressed: (){
+                                setState(() {
+                                  _isObscure2=!_isObscure2;
+                                });
+                            },
+                          ),
+
+                          border: OutlineInputBorder(
+                              borderRadius:  BorderRadius.all(
+                                   Radius.circular(10.0))),
+                        ),
+                        obscureText: _isObscure2,
                       ),
-                    )
-                  ],
+
+
+                    ],
+                  ),
                 ),
-              ),
 
-              Container(
-                padding: EdgeInsets.only(left: 15.0, right: 15.0, top: 15.0),
-                height: 60.0,
-                child: TextButton(
-                  onPressed: () {},
+                if(updateFailed) ...[
+                  Text("Sorry. Password reset failed!", style: GoogleFonts.montserrat(
+                    color: Colors.red,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  )
+                ],
+                if(notMatched) ...[
+                  Text("Password Not matched!", style: GoogleFonts.montserrat(
+                    color: Colors.red,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  )
+                ],
+
+
+
+                SizedBox(height: 35,),
+
+                TextButton(
+                  onPressed: () {
+                    print("Conf:"+ confPassword.toString());
+                    print("password:"+ password.toString());
+
+                    if(_formKey.currentState.validate()){
+
+                      _formKey.currentState.save();
+
+                      //Validate Password Not matched
+                      if(password != confPassword){
+                        setState(() {
+                          notMatched=true;
+                        });
+                      }else{
+                        //Password matched
+                        updatePassword(context);
+                      }
+                    }
+
+                  },
                   child: Text(
-                    "Change Passwords",
+                    "Reset Password",
                     style: TextStyle(
                       color: Colors.white,
-                      fontWeight: FontWeight.w400,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
                   style: ButtonStyle(
                       alignment: Alignment.center,
                       backgroundColor:
-                          MaterialStateProperty.all(Color(0xFF64B5F6)),
+                          MaterialStateProperty.all(Colors.lightBlue),
                       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                           RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10.0)))),
                 ),
-              ),
-            ],
-          ),
-        ));
-  }
-}
 
-class TextWidget extends StatelessWidget {
-  const TextWidget({
-    Key key,
-    @required this.textstring,
-    @required this.paddingtext,
-  }) : super(key: key);
-
-  final String textstring;
-  final EdgeInsets paddingtext;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      padding: paddingtext,
-      child: Text(
-        textstring,
-        style: GoogleFonts.montserrat(
-          color: Colors.white,
-          fontWeight: FontWeight.w200,
-          fontSize: 11.0,
-        ),
-      ),
-    );
-  }
-}
-
-class LoginButton extends StatelessWidget {
-  const LoginButton({Key key,
-      @required this.title,
-      @required this.icon,
-      @required this.color,
-      @required this.paddingbutton})
-      : super(key: key);
-
-  final String title;
-  final FaIcon icon;
-  final Color color;
-  final EdgeInsets paddingbutton;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      //width: 180.0,
-      height: 40.0,
-      padding: paddingbutton,
-      //padding: EdgeInsets.only(left: 5.0, right: 5.0),
-      child: TextButton(
-        onPressed: () {
-          print("clicked ${title}");
-        },
-        child: Row(
-          children: [
-            icon,
-            Padding(padding: EdgeInsets.only(left: 5.0, right: 20.0)),
-            Text(
-              title,
-              style: GoogleFonts.montserrat(
-                fontWeight: FontWeight.w400,
-                color: Colors.white,
-              ),
+              ],
             ),
-          ],
-        ),
-        style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(color),
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0)))),
-      ),
+          ),
+        )
     );
   }
 }
-
