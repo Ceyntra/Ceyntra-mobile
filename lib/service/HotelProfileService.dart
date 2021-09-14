30f39986f84795caddac763ba777676d94026ee7
HiruniHasara <63002515+HiruniHasara@users.noreply.github.com>
2021-09-14 14:02:38 +0530

    added deleteAccount request

--- lib/service/HotelProfileService.dart
+++ lib/service/HotelProfileService.dart
@@ -33,4 +33,9 @@ class HotelProfileService{
     var response = await dio.put('http://10.0.2.2:9092/changePassword', data: passwordDetails);
     return response.data;
   }
+
+  Future<int> removeHotelAccount(uID) async{
+    var response = await dio.delete('http://10.0.2.2:9092/deleteHotelAccount/$uID');
+    return response.data;
+  }
 }
