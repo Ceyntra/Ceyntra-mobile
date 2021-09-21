976e1c26cca4c229a1d02aa06776c592f754f476
Deeghayu Fernando <52172635+deefernando6@users.noreply.github.com>
2021-09-21 09:58:48 +0530

    Update animalDetection.dart

--- lib/views/screens/hamburgerScreens/animalDetection.dart
+++ lib/views/screens/hamburgerScreens/animalDetection.dart
@@ -1,1 +1,307 @@
 
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ceyntra_mobile/views/widgets/greenTagWidget.dart';
+import 'package:tflite/tflite.dart';
+
+
+class AnimalDetectionScreen extends StatefulWidget {
+  // const AnimalDetectionScreen({ Key? key }) : super(key: key);
+  final Function pressed;
+  final bool isPressed;
+  final Function setNull;
+  AnimalDetectionScreen({this.pressed, this.isPressed, this.setNull});
+
+  @override
+  _AnimalDetectionScreenState createState() => _AnimalDetectionScreenState();
+}
+
+class _AnimalDetectionScreenState extends State<AnimalDetectionScreen> {
+  File imageFile;
+  List _results;
+  String queryResults;
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Future _imgFromCamera() async {
+    final pickedFile = await ImagePicker().getImage(source: ImageSource.camera);
+    imageClassification(pickedFile);
+    setState(() {
+      imageFile = File(pickedFile.path);
+    });
+  }
+
+  Future _imgFromGallery() async {
+    final pickedFile =
+        await ImagePicker().getImage(source: ImageSource.gallery);
+    imageClassification(pickedFile);
+    setState(() {
+      imageFile = File(pickedFile.path);
+    });
+  }
+
+  void _showPicker(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Container(
+              child: Wrap(
+                children: <Widget>[
+                  ListTile(
+                      leading: Icon(
+                        Icons.photo_library,
+                        color: Colors.black,
+                      ),
+                      title: Text('Photo Library'),
+                      onTap: () {
+                        _imgFromGallery();
+                        Navigator.of(context).pop();
+                      }),
+                  ListTile(
+                    leading: Icon(
+                      Icons.photo_camera,
+                      color: Colors.black,
+                    ),
+                    title: Text('Camera'),
+                    onTap: () {
+                      _imgFromCamera();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff192537),
+      appBar: AppBar(
+        title: Text(
+          "Ceyntra animal detection",
+          style: GoogleFonts.montserrat(fontSize: 18),
+        ),
+        leading: widget.isPressed
+            ? InkWell(
+                onTap: () {
+                  widget.setNull();
+
+                  widget.pressed(0.0, 0.0, 1.0, false);
+                },
+                child: Icon(Icons.arrow_back))
+            : InkWell(
+                onTap: () {
+                  widget.setNull();
+
+                  widget.pressed((MediaQuery.of(context).size.width / 100) * 70,
+                      70.0, 0.8, true);
+                },
+                child: Icon(
+                  Icons.menu,
+                )),
+        backgroundColor: Color(0xff192537),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              child: GreenTagWidget(
+                title: "Photo",
+              ),
+            ),
+            imageFile != null
+                ? Container(
+                    margin: EdgeInsets.only(right: 10, left: 10),
+                    height: 200,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.withOpacity(0.5),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                      image: DecorationImage(
+                        image: FileImage(imageFile),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Stack(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 140),
+                          height: 60,
+                          decoration: BoxDecoration(
+                            // color: Colors.black.withOpacity(0.4),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Material(
+                                color: Colors.transparent,
+                                child: Ink(
+                                  decoration: const ShapeDecoration(
+                                    color: Colors.white,
+                                    shape: CircleBorder(),
+                                  ),
+                                  child: IconButton(
+                                    icon: const Icon(Icons.camera_alt_outlined),
+                                    // color: Colors.white,
+                                    onPressed: _imgFromCamera,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 5),
+                              Material(
+                                color: Colors.transparent,
+                                child: Ink(
+                                  decoration: const ShapeDecoration(
+                                    color: Colors.white,
+                                    shape: CircleBorder(),
+                                  ),
+                                  child: IconButton(
+                                    icon: const Icon(
+                                        Icons.photo_library_outlined),
+                                    // color: Colors.white,
+                                    onPressed: _imgFromGallery,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 5),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                : Container(
+                    margin: EdgeInsets.only(right: 10, left: 10),
+                    height: 200,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.2),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                    ),
+                    child: IconButton(
+                        onPressed: () {
+                          _showPicker(context);
+                        },
+                        icon: Icon(
+                          Icons.add_photo_alternate_rounded,
+                          size: 80.0,
+                          color: Colors.white,
+                        )),
+                  ),
+            Container(
+              alignment: Alignment.centerLeft,
+              child: GreenTagWidget(
+                title: "Result",
+              ),
+            ),
+            imageFile != null
+                ? Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(right: 10, left: 10),
+                    // height: 200,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      // color: Colors.grey.withOpacity(0.5),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                    ),
+
+                    child: Column(
+                      children: _results != null
+                          ? _results.map((result) {
+                              return Card(
+                                color: Color(0xff192537),
+                                child: Container(
+                                  child: Text(
+                                    "${result["label"]} (Confidence -  ${result["confidence"].toStringAsFixed(2)})",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              );
+                            }).toList()
+                          : [],
+                    ),
+                  )
+                : Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(right: 10, left: 10),
+                    // height: 200,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      // color: Colors.grey.withOpacity(0.5),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                    ),
+
+                    child: Text(
+                      'Please select the image to view results',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future loadModel() async {
+    Tflite.close();
+    String res;
+    res = await Tflite.loadModel(
+      model: "assets/mobilenet_v1_1.0_224.tflite",
+      labels: "assets/mobilenet_v1_1.0_224.txt",
+    );
+    print(res);
+  }
+
+  Future imageClassification(var image) async {
+    // Run tensorflowlite image classification model on the image
+    final List results = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 1,
+      threshold: 0.05,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+    setState(() {
+      _results = results;
+    });
+  }
+
+  void queryPage() async {
+    var wiki = Wikipedia();
+    var resultss = await wiki.search('hello world');
+    var queryResults = await wiki.getPage(resultss.searchResults[0].title);
+    print(queryResults);
+  }
+}
